0a6b78c802157427983812005826273b59fe05ed
Simo <153776287+SimoHypers@users.noreply.github.com>
2025-05-09 15:21:58 +0400

    Added the html template, will soon start working on showcasing user match info

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from supabase_client import supabase
-from fastapi import FastAPI, HTTPException, Depends, Header
+from fastapi import FastAPI, HTTPException, Depends, Header, Request, Form
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from routers import auth, summoners
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,9 +19,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"message": "League Tracker API is up and running"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "data": None, "error": None})
+
+@app.post("/", response_class=HTMLResponse)
+async def search_summoner(request: Request, summoner_name: str = Form(...)):
+    response = supabase.table("summoner_profiles").select("*").eq("summoner_name", summoner_name).execute()
+
+    if not response.data:
+        return templates.TemplateResponse("index.html", {
+            "request": request,
+            "data": None,
+            "error": "Summoner not found"
+        })
+    
+    summoner = response.data[0]
+    puuid = summoner["puuid"]
+
+    match_response = supabase.table("player_matches").select("match_id").eq("puuid", puuid).execute()
+    match_ids = [match["match_id"] for match in match_response.data]
+
+    data = {
+        "summoner_name": summoner["summoner_name"],
+        "matches": match_ids
+    }
+
+    return templates.TemplateResponse("index.html", {"request": request, "data": data, "error": None})
 
 #Including Routers
 app.include_router(auth.router)
